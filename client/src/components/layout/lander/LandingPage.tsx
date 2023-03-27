c1d27833e7237d210e096c42670e03072960a535
Nick <nhaumer@mail.com>
2023-03-27 18:29:14 +0200

    Changes Final

--- client/src/components/layout/lander/LandingPage.tsx
+++ client/src/components/layout/lander/LandingPage.tsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
-import { Typography } from '@mui/material'
+import { Button, Checkbox, FormControlLabel, Link, Paper, Typography } from '@mui/material'
 
 export default function LandingPage() {
+    const navigate = useNavigate()
+    const [skipLandingPage, setSkipLandingPage] = useState((JSON.parse(localStorage.getItem('skip-page') as string) === 'true') ? 'true' : 'false')
 
-    const [skipLandingPage, setSkipLandingPage] = useState((JSON.parse(localStorage.getItem('landing-page') as string) === 'skip') ? true : false)
+    const handleChange = () => {
+      const status = (skipLandingPage === 'true') ? 'false' : 'true'
+      localStorage.setItem('skip-page', JSON.stringify(status))
+      setSkipLandingPage(status)
+    }
 
+    useEffect(() => {
+      if (skipLandingPage === 'true') {
+        navigate('/home')
+      }
+    }, [skipLandingPage, navigate])
 
-      const myStatus = JSON.parse(localStorage.getItem('landing-page') as string) || false
-
-      console.log(myStatus)
-    
-      const handleToggle = () => {
-        
-        //setSkipLandingPage(localStorage.setItem('landing-page', JSON.stringify('skip')))
-
-      };
-    
   return (
     <Box
       sx={{
@@ -34,9 +36,17 @@ export default function LandingPage() {
     >
       <CssBaseline />
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-            <Typography>Test {myStatus.toString()}</Typography>
-            <button onClick={handleToggle}>Toggle</button>
+        <Grid item xs={12} md={8} lg={8}>
+          <Paper sx={{ p: 1, display: 'flex', flexDirection: 'column', height: 280 }}>
+            <Typography sx={{ mt: 4 }} variant="h4" component="h2">You made it to my final MasterClass Project!</Typography>
+            <Typography sx={{ mt: 2 }} >-= This is a demo application with prices from 2022 which is limited to Berlin =-</Typography>
+            <Typography sx={{ mt: 2 }} >Full details about the application you will find on GitHub (<Link href="https://github.com/CodesShowcase/masterproject"> =&gt; Link </Link>).</Typography>
+            <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" className="landing-grid">
+            <Button sx={{ mt: 2 }}variant="contained" href="/home">Take me to the application</Button>
+              <FormControlLabel sx={{ mt: 1, ml: 4 }} control={<Checkbox checked={(skipLandingPage === 'true') ? true : false} onChange={handleChange}
+              inputProps={{ 'aria-label': 'controlled' }} />} label="Check here if you want to skip this page and go straight to the application." />
+            </Grid>
+          </Paper>
         </Grid>
       </Container>
     </Box>
--- client/src/utils/constants/index.ts
+++ client/src/utils/constants/index.ts
@@ -1,2 +1,2 @@
-export const GOOGLE_API = 'AIzaSyDaZkdl5KuI87avK1FFu7Id24EsfKN3l0o'
+export const GOOGLE_API = '<GOOGLE MAPS API KEY>'
 export const REST_API = 'http://localhost:3080'
